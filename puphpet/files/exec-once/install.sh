0eea17f8af8a0b9651eb41a67fc78d68fd3af3c2
Chris Hougard <hougardc@msoe.edu>
2015-04-04 02:20:46 -0500

    Fixed some mistakes

--- puphpet/files/exec-once/install.sh
+++ puphpet/files/exec-once/install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-ln -s
 cd /opt/concrete5/web/concrete
 sudo -H -u vagrant sh -c 'COMPOSER_DISCARD_CHANGES=1 composer install'
 sudo -H -u vagrant sh -c 'php /opt/concrete5/cli/install-concrete5.php --db-server=localhost --db-username=concrete5 --db-password=concrete5developer --db-database=concrete5 --admin-email=admin@example.com --admin-password=concrete5developer --target=/var/www/concrete5 --site="Test Site"  --reinstall=yes'
